73db8a959da3e5afdacf25807090fe1e26f51484
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-15 15:07:01 -0400

    create an array to catch the user input

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+// An array to catch the user input
+const employeeList = [];
+
 // Import Inquirer
 const inquirer = require('inquirer');
+

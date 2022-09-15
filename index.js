97e3e558d6cf4008f70aad006c7d4972f987efe1
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-15 15:57:04 -0400

    set index.js to run the Roster.js file

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const Employee = require('./lib/Employee');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Roster = require('./lib/Roster');
 
-// An array to catch the user input
-const employeeList = [];
+const roster = new Roster();
 
-// Import Inquirer
-const inquirer = require('inquirer');
-
+roster.getManager();

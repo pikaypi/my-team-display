33bda593af7b20da8078292fd6c163ac7f5620d2
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-15 12:58:58 -0400

    install inquirer

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+// Import Inquirer
+const inquirer = require('inquirer')

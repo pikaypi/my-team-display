48ed1f4b5b56f8a586c202c832e13c59a87e5c4d
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-19 13:33:22 -0400

    add anchor tag to engineer github

--- src/template.js
+++ src/template.js
@@ -21,7 +21,7 @@ const generateCard = (employee) => {
                         </div>
                         <p class="bg-slate-300 mr-1 ml-1 w-2/3 justify-self-center p-2">Id: ${employee.id}</p>
                         <p class="bg-slate-300 mr-1 ml-1 w-2/3 justify-self-center p-2">Email: <a href="mailto: ${employee.email}">${employee.email}</a></p>
-                        <p class="bg-slate-300 mr-1 ml-1 w-2/3 justify-self-center p-2">GitHub: ${employee.github}</p>
+                        <p class="bg-slate-300 mr-1 ml-1 w-2/3 justify-self-center p-2">GitHub: <a href="https://www.github.com/${employee.github}" target="_blank">${employee.github}</a></p>
                     </div>`
         case employee.getRole() === 'Intern':
             return `<div class="bg-yellow-500 col-span-1 grid rounded-t-lg gap-y-1 pb-2 drop-shadow-lg">
